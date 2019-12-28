6df21117c2cec69b937699448028c30c639e437b
Yanggle Yang <yanggle.yang@ecitele.com>
2019-12-28 17:08:32 +0800

    Eureka Server with SSL

---
+++ springcloud-eureka-server-ssl/src/main/java/com/yanggle/springcloud/eureka/SpringCloudEurekaServerSSLApplication.java
@@ -0,0 +1,16 @@
+package com.yanggle.springcloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringCloudEurekaServerSSLApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringCloudEurekaServerSSLApplication.class, args);
+    }
+
+}
