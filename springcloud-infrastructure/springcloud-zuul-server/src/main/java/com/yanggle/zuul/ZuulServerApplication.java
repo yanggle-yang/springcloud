936712ea67809b96f16e5353623eaa6645d23e92
Yanggle Yang <yanggle.yang@ecitele.com>
2019-12-29 14:38:44 +0800

    Zuul Server

---
+++ springcloud-infrastructure/springcloud-zuul-server/src/main/java/com/yanggle/zuul/ZuulServerApplication.java
@@ -0,0 +1,16 @@
+package com.yanggle.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy    //开启Zuul功能
+@EnableEurekaClient //开启Eureka Client功能
+@SpringBootApplication
+public class ZuulServerApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(ZuulServerApplication.class,args);
+    }
+}
