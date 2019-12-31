581d26f4a92a8e30a5ecb46b01e46a8187b07d68
Yanggle Yang <yanggle.yang@ecitele.com>
2019-12-31 11:12:04 +0800

    improve

--- springcloud-infrastructure/springcloud-eureka-server-standalone/src/main/java/com/yanggle/eureka/SpringCloudEurekaServerStandaloneApplication.java
+++ springcloud-infrastructure/springcloud-eureka-server-standalone/src/main/java/com/yanggle/eureka/SpringCloudEurekaServerStandaloneApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer //开启Eureka Server
 @SpringBootApplication
-public class SpringCloudEurekaServerApplication {
+public class SpringCloudEurekaServerStandaloneApplication {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(SpringCloudEurekaServerApplication.class, args);
+        SpringApplication.run(SpringCloudEurekaServerStandaloneApplication.class, args);
     }
 }
